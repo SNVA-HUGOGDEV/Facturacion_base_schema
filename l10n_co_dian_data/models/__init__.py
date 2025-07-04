879a9b5da8225d5ff89ac3db3c982e3804948ea3
GitHub Action (Schema Bot) <actions@github.com>
2025-07-04 20:09:44 +0000

    [AUTO] Actualización de esquema para la versión V17

--- l10n_co_dian_data/models/__init__.py
+++ l10n_co_dian_data/models/__init__.py
@@ -4,7 +4,6 @@ from . import l10n_co_account_fiscal_position_party_tax_scheme
 from . import l10n_co_account_invoice_discrepancy_response
 from . import l10n_co_account_invoice_payment_mean
 from . import l10n_co_account_tax_group_type
-from . import l10n_co_base_location
 from . import l10n_co_partner_person_type
 from . import l10n_co_partner_vat
 from . import l10n_co_product_uom
